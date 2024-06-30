756803a92508fa4322bff851457d7a01ea0ec2a8
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-06-30 11:57:57 +0530

    Initial commit

---
+++ Suprime2.0/operator.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    char ch;
+    cout<<"Enter a character:"<<endl;
+    cin>>ch;
+
+
+
+    cout<<"here:"<<ch<<endl;
+
+
+    // int marks;
+    // cout<<"Enter the Marks here"<<endl;
+    // cin>>marks;
+
+
+
+
+    // cout<<"Printing Marks:"<<marks<<endl;
+
+    // int age = 2;
+    // int car = 12;
+    // if(age >=20 || car>1){
+    //     cout<<"Lisence Mil Jayega"<<endl;
+    // }
+
+    // cout<<!age;
+
+    // int a = 20;
+    // int b = 10;
+    // cout<<(a>b)<<endl;
+    // cout<<(a<b)<<endl;
+    // cout<<(a>=b)<<endl;
+    // cout<<(a<=b)<<endl;
+    // cout<<(a==b)<<endl;
+    // cout<<(a!=b)<<endl;
+
+    // int a = 5;
+    // int b = 10;
+    // cout<<a+b<<endl;
+    // cout<<a-b<<endl;
+    // cout<<a*b<<endl;
+    // cout<<a/b<<endl;
+
+}
