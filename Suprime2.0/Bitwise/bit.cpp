1f41f0a6c575012b1ac81c7d7f5c1c4637aa0d19
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-07-07 10:41:15 +0530

    Initial commit

---
+++ Suprime2.0/Bitwise/bit.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a= 2;
+    int b = 3;
+
+    cout<<(a & b)<<endl;
+
+    int num =1;
+    cout<<~num<<endl;
+
+    int c = 5;
+    int d = 5;
+    cout<<(c^d)<<endl;
+
+
+    int e =7;
+    int ans = (e<<1);
+    cout<<ans<<endl;
+
+    return 0;
+
+}
