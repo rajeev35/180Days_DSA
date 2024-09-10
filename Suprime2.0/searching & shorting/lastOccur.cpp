463e20918dba62548302a0fe72cd89989080389d
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-09-10 07:13:57 +0530

    Last Occur

---
+++ Suprime2.0/searching & shorting/lastOccur.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int firstOccurence(int arr[], int n, int target){
+    int s = 0;
+    int e = n -1;
+    int mid = (s+e)/2;
+    int ans = -1;  // -1 esliye ki element nhi mila 
+
+    while(s<=e){
+       if(arr[mid] == target){
+        ans = mid;
+        s = mid + 1;
+       }else if(target > arr[mid]){
+        s = mid + 1;
+       }else if(target<arr[mid]){
+        e = mid - 1;
+       }
+
+       mid = (s+e)/2;
+       
+    }
+    return ans;
+}
+int main(){
+
+    int arr[] = {10,30,30,30,30,30,40,50,60};
+    int n = 9;
+    int target = 30;
+    int ansIndex = firstOccurence(arr,n,target);
+    if(ansIndex == -1){
+        cout<<"Element not found"<<endl;
+
+    }else{
+        cout<<"Element found at index "<<ansIndex<<endl;
+    }
+
+    
+}
